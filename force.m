73bc6f62e254836f143f127c9c4feb09baa09bf0
Tomaso <tomasodp@gmail.com>
2021-11-14 21:50:56 +0100

    clean

--- force.m
+++
@@ -1,6 +0,0 @@
-function [output] = force(axis,nominal,angle)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-output = round(nominal.*cos(angle)+cross(axis,nominal).*sin(angle)+axis.*dot(axis,nominal)*(1-cos(angle)),3);
-end
-
--- unitvect.m
+++
@@ -1,6 +0,0 @@
-function [output] = unitvect(A,B)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-output = (B-A)./norm(B-A);
-end
-
