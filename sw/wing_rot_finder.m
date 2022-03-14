4b8f16173efe4e3d8ef4c213f9872c4a031bedcd
Tomaso <tomasodp@gmail.com>
2022-03-14 21:11:13 +0100

    effectiveness estimator 4

--- sw/wing_rot_finder.m
+++ sw/wing_rot_finder.m
@@ -3,13 +3,13 @@ function [time] = wing_rot_finder(ac_data)
 %   Detailed explanation goes here
 check = 0;
 time = [];
-x = ac_data.ROT_WING_CONTROLLER.wing_angle_deg;
-sp = ac_data.ROT_WING_CONTROLLER.wing_angle_deg_sp;
+x = ac_data.EFF_FULL_INDI.wing_angle_deg;
+sp = ac_data.EFF_FULL_INDI.wing_angle_deg_sp;
 diff = abs(sp-x);
 mn = mean(diff);
 for i = 2:size(x,1)
     if diff(i)> 5*mn && abs(diff(i)-diff(i-1))> abs(0.8*diff(i))
-        time = [time, ac_data.ROT_WING_CONTROLLER.timestamp(i)];
+        time = [time, ac_data.EFF_FULL_INDI.timestamp(i)];
     end
 end
 end
